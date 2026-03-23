7b98c969092db7119601afe3512b67896847c84f
Cosmin Cojocar <cosmin@cojocar.ch>
2026-03-23 09:27:39 +0100

    Update .config/nvim/lua/layers/editor.lua

--- dot_config/nvim/lua/layers/editor.lua
+++ dot_config/nvim/lua/layers/editor.lua
@@ -17,6 +17,14 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = { hidden = true },
+          live_grep = {
+            additional_args = { "--hidden", "--glob", "!.git" },
+          },
+        },
+      })
       local t = require("telescope.builtin")
       vim.keymap.set("n", "<Space>ff", t.find_files,  { desc = "Find files" })
       vim.keymap.set("n", "<Space>fg", t.live_grep,   { desc = "Live grep" })
